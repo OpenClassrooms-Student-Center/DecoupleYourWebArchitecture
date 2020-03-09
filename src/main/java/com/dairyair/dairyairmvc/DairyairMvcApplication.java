0c0f1b9a82efd5106a3caca420a3640c4f74b774
glenw <glenw@efinteractive.com>
2020-03-08 18:46:56 -0700

    index.html and logo

---
+++ src/main/java/com/dairyair/dairyairmvc/DairyairMvcApplication.java
@@ -0,0 +1,13 @@
+package com.dairyair.dairyairmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DairyairMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DairyairMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dairyair/dairyairmvc/DairyairMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dairyair.dairyairmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DairyairMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
