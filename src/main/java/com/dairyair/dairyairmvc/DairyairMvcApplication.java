eee146b356e6b751139bc65c468e2ee3a5b5b686
glenw <glenw@efinteractive.com>
2020-03-08 19:01:00 -0700

    Client data repository

--- src/main/java/com/dairyair/dairyairmvc/DairyairMvcApplication.java
+++ src/main/java/com/dairyair/dairyairmvc/DairyairMvcApplication.java
@@ -1,9 +1,19 @@
 package com.dairyair.dairyairmvc;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages={"com.dairyair.dairyairmvc"})
+@EnableJpaRepositories(basePackages="com.dairyair.dairyairmvc.repositories")
+@EnableTransactionManagement
+@EntityScan(basePackages="com.dairyair.dairyairmvc.entities")
 public class DairyairMvcApplication {
 
 	public static void main(String[] args) {
