92ab12f76c47904913b1dd942a2bb981acc82ca9
glenw <glenw@efinteractive.com>
2020-03-09 15:54:42 -0700

    added clients that owe button

--- src/main/java/com/dairyair/dairyairmvc/repositories/ClientRepository.java
+++ src/main/java/com/dairyair/dairyairmvc/repositories/ClientRepository.java
@@ -1,5 +1,8 @@
 package com.dairyair.dairyairmvc.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +10,6 @@ import com.dairyair.dairyairmvc.entities.Client;
 
 @Repository
 public interface ClientRepository extends CrudRepository<Client, Long>  {
+	@Query("SELECT c FROM Client c WHERE c.outstandingBalance > 0.0")
+	List<Client> findByOustandingBalanceGreaterThan(double value);
 }
